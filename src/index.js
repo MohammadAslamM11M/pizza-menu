174afb0d622cff64c28083eb12dabc4b1c6b72db
MohammadAslamM11M <aslam071595@gmail.com>
2026-03-29 09:46:45 +0530

    final styling and condition for sold out added

--- src/index.js
+++ src/index.js
@@ -114,17 +114,14 @@ function Footer() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
-
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
-      <p>{pizzaObj.soldOut}</p>
     </li>
   );
 }
