03ea7c977f7ee3f9354b7e1fd46154542a169d4b
MohammadAslamM11M <aslam071595@gmail.com>
2026-03-29 09:33:32 +0530

    react fragment added

--- src/index.js
+++ src/index.js
@@ -73,11 +73,17 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza key={pizza.name} pizzaObj={pizza} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all deslicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza key={pizza.name} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on oue menu. Please come back later :)</p>
       )}
